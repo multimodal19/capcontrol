1e70efcefdc7dcc0f009080ccb72cb33832c0312
Martin Disch <martindisch@gmail.com>
2019-03-15 10:00:04 +0100

    Change demo text

--- camera/camwindow.py
+++ camera/camwindow.py
@@ -21,7 +21,7 @@ else:
 font = cv2.FONT_HERSHEY_SIMPLEX
 while rval:
     cv2.putText(
-        frame, "OpenCV", (0, 100), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+        frame, "Whatever", (0, 100), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.ellipse(frame, (150, 200), (100, 50), 0, 0, 180, 255, -1)
     cv2.circle(frame, (350, 200), 63, (0, 0, 255), -1)
     cv2.imshow("preview", frame)
